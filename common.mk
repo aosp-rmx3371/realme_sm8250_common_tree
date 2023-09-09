c00620c1e4e34fd2fc0beb868f8f2ca882b99d4b
Abhishek Shukla <91975183+abhis699@users.noreply.github.com>
2023-09-09 16:24:07 +0530

    sm8250-common: Build QTI Power HALs & declared Ipower into FCM

--- common.mk
+++ common.mk
@@ -358,6 +358,11 @@ PRODUCT_PACKAGES += \
     libqti-perfd-client
 
 # Power
+PRODUCT_PACKAGES += \
+    android.hardware.power-service-qti \
+    android.hardware.power@1.2.vendor \
+    vendor.qti.hardware.perf@2.2.vendor
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/power/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
