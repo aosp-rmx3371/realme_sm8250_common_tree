971537dca1d5e2588169419d4399b2729d26e311
Abhishek Shukla <91975183+abhis699@users.noreply.github.com>
2023-09-10 17:54:33 +0530

    sm8250-common: Build OPlusExtras

--- common.mk
+++ common.mk
@@ -336,6 +336,10 @@ PRODUCT_PACKAGES += \
     libOmxVdec \
     libstagefrighthw
 
+# OPlusExtras
+PRODUCT_PACKAGES += \
+    OPlusExtras
+
 # Overlays
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
 
