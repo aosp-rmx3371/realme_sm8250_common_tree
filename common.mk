f93b99fc80bf390d922196722969f4d8e6a042fa
Abhishek Shukla <91975183+abhis699@users.noreply.github.com>
2023-09-11 09:17:03 +0530

    sm8250-common: Build oplus camera

--- common.mk
+++ common.mk
@@ -136,6 +136,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# Camera
+$(call inherit-product-if-exists, vendor/oplus/camera/opluscamera.mk)
+
 # Charateristics
 PRODUCT_CHARACTERISTICS := nosdcard
 
