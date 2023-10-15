63e12719768544ea73d10a8b0b6ace6c2cd75471
Linux4 <Linux4@users.noreply.github.com>
2023-10-15 07:50:28 +0000

    sm8250-common: Patch widevine to load libprotobuf-cpp-full-3.9.1 * Patch widevine to load full variant as there's no lite vendorcompat variant
    Signed-off-by: yograjfire18 <yograjfire18@gmail.com>

--- common.mk
+++ common.mk
@@ -385,6 +385,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
     $(LOCAL_PATH)/configs/power/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
+# Protobuf
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full \
+    libprotobuf-cpp-full-3.9.1-vendorcompat
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson \
@@ -400,7 +405,6 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.deprecated@1.0.vendor
 
 PRODUCT_PACKAGES += \
-    libprotobuf-cpp-full \
     librmnetctl \
     libxml2
 
