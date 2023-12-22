37ca82f773bde04fb9ffe9b49e9ff739b62416be
Abhishek Shukla <91975183+abhis699@users.noreply.github.com>
2023-12-22 15:30:19 +0530

    sm8250-common: remove Ipower from manifest
    getDeviceHalManifest: status from fetching VINTF information: -2147483648
    getDeviceHalManifest: -2147483648 VINTF parse error: Cannot add manifest fragment /vendor/etc/vintf/manifest/power.xml: HAL "android.hardware.power" has a conflict: Conflicting FqInstance: IPower/default
    (@3) (from /vendor/etc/vintf/manifest.xml) vs. IPower/default (@3) (from /vendor/etc/vintf/manifest/power.xml). Check whether or not multiple modules providing the same HAL are installed.

--- common.mk
+++ common.mk
@@ -381,9 +381,6 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
-    android.hardware.power@1.2.vendor \
-    vendor.qti.hardware.perf@2.2.vendor \
     android.hardware.power-service.oplus-libperfmgr
 
 PRODUCT_COPY_FILES += \
